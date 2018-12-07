e7f51b777aaa00244813cb55757bfe97e076434b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2018-12-07 17:02:02 -0600

    Initial Mono.Cecil benchmarks

---
+++ Benchmarks/Cecil.cs
@@ -0,0 +1,60 @@
+﻿using BenchmarkDotNet.Attributes;
+using Java.Interop.Tools.Cecil;
+using Mono.Cecil;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Benchmarks
+{
+	public class Cecil
+	{
+		readonly string [] assemblies;
+
+		public Cecil ()
+		{
+			var bin = Path.GetDirectoryName (GetType ().Assembly.Location);
+			var assembliesDir = Path.Combine (bin, "..", "..", "Assemblies");
+			assemblies = Directory.GetFiles (assembliesDir, "*.dll");
+		}
+
+		[Benchmark]
+		public void Defaults ()
+		{
+			using (var resolver = new DirectoryAssemblyResolver (Log, loadDebugSymbols: false)) {
+				IterateAssemblies (resolver);
+			}
+		}
+
+		[Benchmark]
+		public void InMemory ()
+		{
+			var rp = new ReaderParameters {
+				InMemory = true
+			};
+			using (var resolver = new DirectoryAssemblyResolver (Log, loadDebugSymbols: false, loadReaderParameters: rp)) {
+				IterateAssemblies (resolver);
+			}
+		}
+
+		void Log (TraceLevel level, string message) { }
+
+		void IterateAssemblies (DirectoryAssemblyResolver resolver)
+		{
+			foreach (var assemblyFile in assemblies) {
+				var assembly = resolver.Load (assemblyFile);
+				foreach (var mod in assembly.Modules) {
+					foreach (var resource in mod.Resources) {
+						var name = resource.Name;
+					}
+					foreach (var attr in mod.CustomAttributes) {
+						var name = attr.AttributeType.Name;
+					}
+					foreach (var type in mod.Types) {
+						var name = type.Name;
+					}
+				}
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Benchmarks
+
+Initial benchmarks for Mono.Cecil:
+
+```
+// * Summary *
+
+BenchmarkDotNet=v0.11.3, OS=Windows 10.0.17763.134 (1809/October2018Update/Redstone5)
+Intel Core i7-7600U CPU 2.80GHz (Kaby Lake), 1 CPU, 4 logical and 2 physical cores
+  [Host]     : .NET Framework 4.7.2 (CLR 4.0.30319.42000), 32bit LegacyJIT-v4.7.3190.0
+  DefaultJob : .NET Framework 4.7.2 (CLR 4.0.30319.42000), 32bit LegacyJIT-v4.7.3190.0
+
+
+   Method |      Mean |    Error |    StdDev |
+--------- |----------:|---------:|----------:|
+ Defaults |  74.45 ms | 1.026 ms | 0.8564 ms |
+ InMemory | 114.33 ms | 2.251 ms | 2.1054 ms |
+
+// * Hints *
+Outliers
+  Cecil.Defaults: Default -> 2 outliers were removed
+  Cecil.InMemory: Default -> 1 outlier  was  removed
+
+// * Legends *
+  Mean   : Arithmetic mean of all measurements
+  Error  : Half of 99.9% confidence interval
+  StdDev : Standard deviation of all measurements
+  1 ms   : 1 Millisecond (0.001 sec)
+
+// ***** BenchmarkRunner: End *****
+Run time: 00:00:33 (33.18 sec), executed benchmarks: 2
+```
